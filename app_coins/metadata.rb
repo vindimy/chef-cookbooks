bd63c6b9b13ffc7226f6fc1736beb4853a2fa630
vindimy <vindimy@gmail.com>
2013-03-06 23:15:05 +0000

    Adding app_coins cookbook

---
+++ app_coins/metadata.rb
@@ -0,0 +1,10 @@
+name             'app_coins'
+maintainer       'Dmitriy Vi'
+maintainer_email 'vindimy@gmail.com'
+license          'All rights reserved'
+description      'Installs/Configures app_coins'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends "application"
+depends "application_ruby"
---
+++ app_coins/recipes/default.rb
@@ -0,0 +1,29 @@
+#
+# Cookbook Name:: app_coins
+# Recipe:: default
+#
+# Copyright 2013, vindimy@gmail.com
+#
+# All rights reserved - Do Not Redistribute
+#
+
+application "my_app" do
+  path "/apps/app_coins"
+  owner "root"
+  group "root"
+
+  repository "https://github.com/vindimy/app_coins.git"
+  revision node.chef_environment == "production" ? "production" : "develop"
+
+  # Apply the rails LWRP from application_ruby
+  rails do
+    # Rails-specific configuration. See the README in the
+    # application_ruby cookbook for more information.
+
+  end
+
+  # Apply the passenger_apache2 LWRP, also from application_ruby
+  passenger_apache2 do
+    # Passenger-specific configuration.
+  end
+end
