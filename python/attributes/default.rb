4cf37835698b55b581ffeb400ba4dade6825b078
dv <dv@chef-1.localdomain>
2013-08-14 23:16:30 -0700

    Import python version 1.3.6

--- python/attributes/default.rb
+++ python/attributes/default.rb
@@ -34,6 +34,9 @@ end
 default['python']['binary'] = "#{python['prefix_dir']}/bin/python"
 
 default['python']['url'] = 'http://www.python.org/ftp/python'
-default['python']['version'] = '2.7.1'
-default['python']['checksum'] = '80e387bcf57eae8ce26726753584fd63e060ec11682d1145af921e85fd612292'
+default['python']['version'] = '2.7.5'
+default['python']['checksum'] = '3b477554864e616a041ee4d7cef9849751770bc7c39adaf78a94ea145c488059'
 default['python']['configure_options'] = %W{--prefix=#{python['prefix_dir']}}
+
+default['python']['setuptools_script_url'] = 'https://bitbucket.org/pypa/setuptools/raw/0.8/ez_setup.py'
+default['python']['pip_script_url'] = 'https://raw.github.com/pypa/pip/master/contrib/get-pip.py'
